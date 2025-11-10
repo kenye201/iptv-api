12099662cc00d21b4d308538955a2c536a48a92f
kenye201 <kenye201@outlook.com>
2025-11-10 23:07:44 +0800

    Update cf_push.py

--- output/data/cf_push.py
+++ output/data/cf_push.py
@@ -11,31 +11,31 @@ print = lambda *args, **kwargs: __builtins__.print(*args, **{**kwargs, "flush": 
 CF_ACCOUNTS = [
     {
         "token": os.getenv("CF_TOKEN_1"),
-        "domains": ["2.c.4.f.0.7.4.0.1.0.0.2.ip6.arpa","3.c.4.f.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["2.c.4.f.0.7.4.0.1.0.0.2.ip6.arpa","3.c.4.f.0.7.4.0.1.0.0.2.ip6.arpa","yubb.netlib.re","yubd.netlib.re","yube.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_2"),
-        "domains": ["e.5.9.f.0.7.4.0.1.0.0.2.ip6.arpa","a.a.9.d.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["e.5.9.f.0.7.4.0.1.0.0.2.ip6.arpa","a.a.9.d.0.7.4.0.1.0.0.2.ip6.arpa","bubr.netlib.re","bubt.netlib.re","buyg.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_3"),
-        "domains": ["6.b.9.d.0.7.4.0.1.0.0.2.ip6.arpa","e.a.9.d.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["6.b.9.d.0.7.4.0.1.0.0.2.ip6.arpa","e.a.9.d.0.7.4.0.1.0.0.2.ip6.arpa","hohe.netlib.re","hohg.netlib.re","hohj.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_4"),
-        "domains": ["5.b.a.f.0.7.4.0.1.0.0.2.ip6.arpa","a.3.8.f.f.f.0.7.0.0.6.2.ip6.arpa"]
+        "domains": ["5.b.a.f.0.7.4.0.1.0.0.2.ip6.arpa","a.3.8.f.f.f.0.7.0.0.6.2.ip6.arpa","hohc.netlib.re","hohs.netlib.re","hohx.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_5"),
-        "domains": ["3.8.1.8.0.7.4.0.1.0.0.2.ip6.arpa","4.a.8.4.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["3.8.1.8.0.7.4.0.1.0.0.2.ip6.arpa","4.a.8.4.0.7.4.0.1.0.0.2.ip6.arpa","bubf.netlib.re","bubv.netlib.re","bubw.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_6"),
-        "domains": ["c.9.0.4.0.7.4.0.1.0.0.2.ip6.arpa","c.9.8.4.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["c.9.0.4.0.7.4.0.1.0.0.2.ip6.arpa","c.9.8.4.0.7.4.0.1.0.0.2.ip6.arpa","buyp.netlib.re","buyv.netlib.re","buyx.netlib.re"]
     },
     {
         "token": os.getenv("CF_TOKEN_7"),
-        "domains": ["1.b.1.8.0.7.4.0.1.0.0.2.ip6.arpa","2.b.1.8.0.7.4.0.1.0.0.2.ip6.arpa"]
+        "domains": ["1.b.1.8.0.7.4.0.1.0.0.2.ip6.arpa","2.b.1.8.0.7.4.0.1.0.0.2.ip6.arpa","babx.netlib.re","babv.netlib.re","babk.netlib.re"]
     }    
 ]
 
